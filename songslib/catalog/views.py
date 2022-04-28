ae9bacba533b914f3e6e5487b8a9729f002a41e0
Polina <schachowa.paulina@gmail.com>
2022-04-28 17:45:32 +0300

    Creating home page

--- songslib/catalog/views.py
+++ songslib/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Song, Singer
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_songs=Song.objects.all().count()
+    num_singers=Singer.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_songs':num_songs,'num_singers':num_singers, 'num_visits':num_visits},
+    )
