9dc48182bf3d1c5505d115a533d128362a785181
Polina <schachowa.paulina@gmail.com>
2022-04-28 17:18:38 +0300

    Registering models, adding some data into the database

--- songslib/catalog/admin.py
+++ songslib/catalog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Singer, Genre, Song
+
+admin.site.register(Genre)
+
+class SongInline(admin.TabularInline):
+    model = Song
+    extra = 0
+
+# Define the admin class
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [SongInline]
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'singer', 'display_genre')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name','singer', 'genre')
+        }),
+        ('Details', {
+            'fields': ('lyrics', 'chords')
+        }),
+    )
